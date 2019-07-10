fd1942ce45bfafd5047b8a23aa6fe0de26c7dbf7
Djogo Patrão <djogopatrao@gmail.com>
2019-07-09 23:50:39 -0300

    v2 squad legion

--- squad_legion/js/game.js
+++ squad_legion/js/game.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
     var selectedFaction;
     var army=[];
     var totalPoints = 0;
+
+    var maxPoints = 800;
+    var allowedQtds;
+
+
     // carrega dados de facção em json
     $.ajax({
         url:'data/factions.json',
@@ -21,6 +26,18 @@ $(document).ready(function(){
             });
 
 
+        }
+    });
+
+    // carrega faixas de minimo/maximo
+    $.ajax({
+        url:'data/ranges.json',
+        dataType:'json',
+        complete: function(x,r){
+            allowedQtds = x.responseJSON;
+            for( var rank in allowedQtds ) {
+                $('#'+rank+'-range').html("["+allowedQtds[rank].min+"-"+allowedQtds[rank].max+"]");
+            }
         }
     });
 
@@ -48,25 +65,45 @@ $(document).ready(function(){
         $('#select-faction').prop('disabled',true);
         selectedFaction = $('#select-faction').val();
 
-        // carrega combo de facções
-        $('#select-unit')
-            .find('option')
-            .remove()
-            .end()
-            .append('<option value="" default=1>Escolha a unidade...</option>')
-            .val('');
+        $('#units').hide();
+
+        var unitsByRank={};
         $(allUnits).each(function(k,v){
             if ( v.faction == selectedFaction || v.faction == 'Neutral' ) {
                 var display_name = getUnitTitle(v);
-                $('#select-unit').append($('<option />').val(v.name).text(display_name));
+                console.log(v.rank);
+                if ( !unitsByRank[ v.rank ] ) {
+                    unitsByRank[ v.rank ] = []
+                }
+                unitsByRank[ v.rank ].push( {'id':k,'name':display_name} ); 
+                //html += "<li class='list-group-item add-unit' unit-id='"+k+"'>" + v.rank + " - "+display_name + "</li>";
             }
         });
 
+        var html = "<ul class='list-group'>";
+        $(['commander','corps','heavy','operative','special_forces','support']).each(function(k,rank){
+            var img = "<img src='icons/"+rank+".png'/>";
+            $(unitsByRank[rank]).each(function(k2,v2){
+                html += "<li class='list-group-item add-unit-item' unit-id='"+v2.id+"'>"+img+" "+v2.name+"</li>"
+            });
+        });
+        html += "</ul>";
+        $('#units').html(html);
+
+        $('.add-unit-item').on('click',addUnitToArmy);
+
     })
 
-    // ao escolher uma unidade, adiciona ela à cambada
+    // ao escolher uma unidade, exibe combo de unidades e esconde exército
     $('#add-unit-button').on('click',function(e){
-        var unit = getUnitDataByName($('#select-unit').val());
+        $('#units').show();
+        $('#army').hide();
+    });
+
+    addUnitToArmy = function(e){
+        // codigo a rodar quando escolhe uma unidade
+        var unit_id = $(e.currentTarget).attr('unit-id');
+        var unit = allUnits[unit_id];
         if ( !unit ) { throw "Não achei essa unidade! Eita."; }
         var newUnit = cloneObject( unit );
         // TODO testar se é unidade unica, e se já existe
@@ -74,7 +111,9 @@ $(document).ready(function(){
         newUnit.equipped_upgrades = []
         army.push(newUnit);
         renderArmy();
-    });
+        $('#units').hide();
+        $('#army').show();
+    };
 
     // pendura um evento onclick para todos os links de upgrade
     hookEventsForUpgradesLinks = function() {    
@@ -137,6 +176,8 @@ $(document).ready(function(){
     renderArmy = function() {
         var html="";
         totalPoints = 0;
+        totalUnitsByRank =  { "commander": 0, "corps": 0, "heavy": 0, "operative": 0, "special_forces": 0, "support": 0 }
+
         $(army).each(function(k,v){
             var card_title = getUnitTitle(v);
             var card_subtitle = getUnitSubTitle(v);
@@ -155,9 +196,20 @@ $(document).ready(function(){
             });
             html += "</div></span>";
             totalPoints += v.cost;
+            totalUnitsByRank[v.rank]++;
         });
         $('#army').html(html);
-        $('#army-total-cost').html(totalPoints);
+        $('#army-total-cost').html(totalPoints + "/" + maxPoints);
+        for (var r in totalUnitsByRank){
+            $('#'+r+'-qtd').html(totalUnitsByRank[r]);
+
+            if ( totalUnitsByRank[r] < allowedQtds[r].min || totalUnitsByRank[r] > allowedQtds[r].max ) {
+                $('#'+r+'-qtd').addClass('text-danger');
+            } else {
+                $('#'+r+'-qtd').addClass('text-dark');
+            }
+
+        };
         hookEventsForUpgradesLinks();
     };
 
